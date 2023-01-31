376d11e2c83ea69f5b5e11ccfc882d7b3fbb1753
noobdy <htommyf12@gmail.com>
2023-01-31 22:38:27 +0800

    added font awesome icons

--- src/lib/markdown/CodeBlock.svelte
+++ src/lib/markdown/CodeBlock.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import { marked } from 'marked';
     import hljs from 'highlight.js/lib/common'; // this should cover most languages
-
     import 'highlight.js/styles/github-dark.css';
+
+    import FAIcon from 'svelte-fa';
+    import { faClone, faCircleCheck, faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 
     export let text: string;
     export let lang: string;
@@ -98,55 +100,16 @@
                 {/if}
             </div>
             {#if copyStatus === 'default'}
-                <button class="p-2 text-slate-600 hover:text-slate-200">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M16.5 8.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v8.25A2.25 2.25 0 006 16.5h2.25m8.25-8.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-7.5A2.25 2.25 0 018.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 00-2.25 2.25v6"
-                        />
-                    </svg>
+                <button class="p-3 text-slate-600 hover:text-slate-200">
+                    <FAIcon icon={faClone} class="text-xl" />
                 </button>
             {:else if copyStatus === 'success'}
-                <button class="p-2 text-emerald-400">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                    </svg>
+                <button class="p-3 text-emerald-400">
+                    <FAIcon icon={faCircleCheck} class="text-xl" />
                 </button>
             {:else}
-                <button class="p-2 text-red-500">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                        />
-                    </svg>
+                <button class="p-3 text-red-500">
+                    <FAIcon icon={faCircleXmark} class="text-xl" />
                 </button>
             {/if}
         </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import '../app.css';
+    import FAIcon from 'svelte-fa';
+    import { faInstagram, faDiscord, faGithub } from '@fortawesome/free-brands-svg-icons';
 </script>
 
 <div class="container mt-10 mx-auto h-full">
@@ -34,8 +36,30 @@
     <slot />
 
     <footer class="my-5">
-        <div class="flex justify-center">
-            <span> NTUT-CINC 2023 </span>
+        <div class="flex flex-col items-center">
+            <div class="pb-3">
+                <span class="text-slate-100">NTUT-CINC 2023</span>
+            </div>
+            <div class="flex items-center">
+                <a
+                    href="https://discord.gg/Sh5Rhj8ghn"
+                    class="w-6 mx-2 text-slate-400 hover:text-slate-100"
+                >
+                    <FAIcon icon={faDiscord} class="" />
+                </a>
+                <a
+                    href="https://github.com/NTUT-CINC/"
+                    class="mx-2 text-slate-400 hover:text-slate-100"
+                >
+                    <FAIcon icon={faGithub} class="text-xl" />
+                </a>
+                <a
+                    href="https://www.instagram.com/ntut_cinc/"
+                    class="mx-2 text-slate-400 hover:text-slate-100"
+                >
+                    <FAIcon icon={faInstagram} class="text-xl" />
+                </a>
+            </div>
         </div>
     </footer>
 </div>
