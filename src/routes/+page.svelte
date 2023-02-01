e3cfafe3782bdc1d1360cd139c0a07020386b420
noobdy <htommyf12@gmail.com>
2023-02-01 12:09:14 +0800

    experiment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,12 @@
 <div class="flex">
     <div class="w-6/12" />
     <div class="w-6/12">
-        <h1
-            class="mt-40 bg-gradient-to-br from-yellow-600 to-red-600 bg-clip-text font-mono text-[150px] font-bold leading-none text-transparent"
-        >
+        <h1 class="mt-40 font-mono text-[150px] font-bold leading-none text-emerald-400">
             NTUT<br />
             CINC
         </h1>
-        <p class="ml-3 font-mono text-xl">CINC is not Cybercriminal</p>
+        <div class="w-fit rounded bg-pink-600 px-4 py-1">
+            <p class="font-mono text-xl">CINC is not Cybercriminal</p>
+        </div>
     </div>
 </div>
