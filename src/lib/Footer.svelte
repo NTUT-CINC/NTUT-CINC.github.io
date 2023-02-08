400465153452837022ecb8a6731fbdc3aa8dcfbb
noobdy <htommyf12@gmail.com>
2023-02-08 12:19:08 +0800

    added aria labels for footer links

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -12,18 +12,21 @@
             <a
                 href="https://discord.gg/Sh5Rhj8ghn"
                 class="mx-2 w-6 text-slate-400 hover:text-slate-100"
+                aria-label="discord-server-invite-link"
             >
                 <FAIcon icon={faDiscord} class="" />
             </a>
             <a
                 href="https://github.com/NTUT-CINC/"
                 class="mx-2 text-slate-400 hover:text-slate-100"
+                aria-label="github-organization-link"
             >
                 <FAIcon icon={faGithub} class="text-xl" />
             </a>
             <a
                 href="https://www.instagram.com/ntut_cinc/"
                 class="mx-2 text-slate-400 hover:text-slate-100"
+                aria-label="instagram-account-link"
             >
                 <FAIcon icon={faInstagram} class="text-xl" />
             </a>
